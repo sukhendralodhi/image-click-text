4c43f4eb3aa4c54c975c5b8757192ca0054c5530
sukhendra <mpct5jan1998@gmail.com>
2023-04-15 17:22:13 +0530

    add one more line js

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const textArea = document.getElementById('res');
 images.forEach((img) => {
     img.addEventListener('click', (e) => {
         console.log(e.target.alt);
-        textArea.value = e.target.alt;
+        textArea.value = `You click on ${e.target.alt}`;
     })
 })
